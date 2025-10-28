55a31bc3612319536ce7093ded09b8ca123254e5
Santiago <laultimadesantiago@gmail.com>
2025-10-28 11:23:28 -0400

    Logica de TicTacToe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -5,52 +5,136 @@ const TURNS = {
   O: 'O'
 }
 
-
-const Square = ({ children,isSelected, updateBoard, index }) => {
+// This is the child component
+const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
-  
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div key={index} className={className}>
-      <span className="cell_content">
-        {index}
-      </span>
+    <div onClick={handleClick}  className={className}>
+      {
+        children
+      }
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-
+// This is the parent component
 function App() {
-  
   const [board, setBoard] = useState(Array(9).fill(null))
-  
-  const [turn, setturn] = useState(TURNS.X)
+  const [turn, setTurn] = useState(TURNS.X)
+  // Null means no winner yet, False means a tie
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] // 0 -> X/O
+        && boardToCheck[a] === boardToCheck[b] 
+        && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+        
+      }
+    }
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  // Here, I'm sending the updateBoard function to the Square component (Child component)
+  const updateBoard = (index) => {
+    
+    // If the square is already filled, do nothing
+    if (board[index] || winner) return
+
+    // Update the board
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // Determine the next turn
+    const newTurn = (turn === TURNS.X) ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // Check for a winner
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+      
+    }
+  }
+
+
+
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
-    <section className="game">
-      {
-        board.map((_, index) => {
-          return (
-            <Square 
-              key={index} 
-              index={index}
-            >
-              {board[index]}
-            </Square>
-          )
-        })
-      }
-    </section>
-    <section className="turn">
-      <Square isSelected= {turn ===TURNS.X}>
-        {TURNS.X}
-      </Square>
-      <Square isSelected= {turn ===TURNS.O}>
-        {TURNS.O}
-      </Square> 
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+                key={index} 
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected= {turn ===TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected= {turn ===TURNS.O}>
+          {TURNS.O}
+        </Square> 
 
-    </section>
+      </section>
+      {
+        winner !== null && (
+          <section className="winner"> 
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? 'Empate'
+                  : 'HA GANADO: '
+                }
+              </h2>
+              <header className="win">
+                { winner && <Square>{winner}</Square> }
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de Nuevo</button>
+              </footer>
+
+            </div>
+          </section>
+        )
+            
+      }
+
     </main>
   )
 }
