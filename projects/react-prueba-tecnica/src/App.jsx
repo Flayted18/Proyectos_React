4bb69a379d9737d97e612531744977242509e110
Flayted18 <laultimadesantiago@gmail.com>
2025-10-31 18:53:12 -0400

    Test finished

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import "./App.css"
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
@@ -6,25 +7,37 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 function App() {  
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  // const [factError, setFactError] = useState()
   
+  // para recuperar la cita al cargar la pagina
+
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const {fact} = data
+    .then(res => {
+      if(!res.ok) throw new Error('Error fetching fact')
+      return res.json()
+    })
+    .then(data => {
+      const {fact} = data
         setFact(fact)
-        const firstWord = fact.split(' ', 3).join(' ')
-        
-        fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
-          .then(res => res.json())
-          .then (response => {
-          const {url} = response
-          setImageUrl(url)
-        })
       })
   }, [])
-  
-  return (
+    
+  // para recuperar la imagen cada vez que cambie la cita
+  useEffect(() => {
+    if (!fact) return
+
+    const firstWord = fact.split(' ', 3).join(' ')
+      
+    fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+      .then(res => res.json())
+      .then (response => {
+      const {url} = response
+      setImageUrl(url)
+    })    
+  }, [fact])
+
+    return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
